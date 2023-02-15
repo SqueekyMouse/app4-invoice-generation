3242319c316bbfc3f6f139de37d4403ba4fa63ef
sreejith <squeeky.mouse@gmail.com>
2023-02-15 22:51:46 +0530

    load excel files into dataframes Sec25

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import pandas as pd
+import glob
+#commit: load excel files into dataframes Sec25
 
-#commit: initial commit Sec25
+filepaths=glob.glob('invoices/*.xlsx')
+# print(filepaths)
 
-print('hello world')
+for filepath in filepaths:
+    df=pd.read_excel(filepath,sheet_name='Sheet 1') # excel files need sheet name!!!
+    print(df)
